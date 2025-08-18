b55d56c7dbc06f4102231577f2946cd5450e976b
Javier Arambula Rascon <ja1473@Javiers-MacBook-Pro.local>
2025-08-17 20:57:54 -0400

    window opens up

---
+++ pong/src/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <raylib.h>
+
+int main() {
+    InitWindow(1000, 600, "c++ - Pong");
+
+    while(!WindowShouldClose()) {
+        BeginDrawing();
+
+
+
+        EndDrawing();
+    }
+
+    CloseWindow();
+    return 0;
+}
